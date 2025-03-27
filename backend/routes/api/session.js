cb2efce31418825ae48190572127cbce37e9c45f
Thomas Yemane <Tomblue246@gmail.com>
2025-03-26 23:18:51 -0700

    finish-phase-05

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+
+router.post(
+  "/",
+  validateLogin,
+  async (req, res, next) => {
     const { credential, password } = req.body;
   
     const user = await User.unscoped().findOne({
@@ -37,7 +54,9 @@ router.post("/", async (req, res, next) => {
     return res.json({
       user: safeUser,
     });
-  });
+  }
+);
+
   
   router.delete("/", (_req, res) => {
     res.clearCookie("token");
@@ -59,5 +78,7 @@ router.get("/", (req, res) => {
     });
   } else return res.json({ user: null });
 });
+
+
 
 module.exports = router;
